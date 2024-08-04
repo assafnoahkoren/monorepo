4943f1c8f7f3d1156e16edd0121cbf34b61eeb43
Assaf Koren <assafnoahkoren@gmail.com>
2024-08-05 00:30:41 +0300

    og tag

--- services/webapp/src/App.tsx
+++ services/webapp/src/App.tsx
@@ -1,6 +1,6 @@
 function App() {
     return (
-        <div style={{textAlign: 'center', marginTop: '100px'}}>
+        <div style={{textAlign: 'center', marginTop: '120px'}}>
             <img src="/alona-icon.png" alt="logo" width="64" height="64" />
             <h2>Under Construction</h2>
         </div>

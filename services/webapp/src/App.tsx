a1ef25583cd55e27f36d86906da0a56316f1e24e
Assaf Koren <assafnoahkoren@gmail.com>
2024-08-05 21:58:16 +0300

    added unocss

--- services/webapp/src/App.tsx
+++ services/webapp/src/App.tsx
@@ -1,6 +1,7 @@
 function App() {
+
     return (
-        <div style={{textAlign: 'center', marginTop: '120px'}}>
+        <div className="w-full flex flex-col justify-center items-center" >
             <img src="/alona-icon.png" alt="logo" width="64" height="64" />
             <h2>Under Construction</h2>
         </div>

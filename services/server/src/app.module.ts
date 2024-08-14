ad2d95fc787332290a0da37a37a73d4fb56a353a
Assaf Koren <assafnoahkoren@gmail.com>
2024-08-15 00:36:35 +0300

    fix build

--- services/server/src/app.module.ts
+++ services/server/src/app.module.ts
@@ -2,12 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CspService } from './csp/csp.service';
-import { SenerioGeneratorService } from './senerio-generator/senerio-generator.service';
 import { ScenarioGeneratorService } from './scenario-generator/scenario-generator.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService, CspService, SenerioGeneratorService, ScenarioGeneratorService],
+  providers: [AppService, CspService, ScenarioGeneratorService],
 })
 export class AppModule {}
